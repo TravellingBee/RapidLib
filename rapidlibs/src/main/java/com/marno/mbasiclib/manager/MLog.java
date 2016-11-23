b78fe575fcd8438acada71c7ea3afb0961692273
李刚 <gang.li@utsoft.cn>
2016-11-23 14:41:42 +0800

    v1.1.5修改MLog不打印的bug

--- rapidlibs/src/main/java/com/marno/mbasiclib/manager/MLog.java
+++ rapidlibs/src/main/java/com/marno/mbasiclib/manager/MLog.java
@@ -1,6 +1,5 @@
 package com.marno.mbasiclib.manager;
 
-import com.marno.mbasiclib.BuildConfig;
 import com.orhanobut.logger.Logger;
 
 /**
@@ -13,27 +12,21 @@ public abstract class MLog {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
 
-    public static final boolean isDebug = BuildConfig.DEBUG;
-
     // 下面四个是默认tag的函数
     public static void i(String msg) {
-        if (isDebug)
-            Logger.i(msg);
+        Logger.i(msg);
     }
 
     public static void d(String msg) {
-        if (isDebug)
-            Logger.d(msg);
+        Logger.d(msg);
     }
 
     public static void e(String msg) {
-        if (isDebug)
-            Logger.e(msg);
+        Logger.e(msg);
     }
 
     public static void json(String jsonStr) {
-        if (isDebug)
-            Logger.json(jsonStr);
+        Logger.json(jsonStr);
     }
 
     // 下面四个是自定义tag的函数
